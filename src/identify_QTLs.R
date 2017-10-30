7c8df67c8d84aa24ed2de67db2f595180f4812f0
ivanov-v-v <vy.vl.ivanov@gmail.com>
2017-10-30 06:45:21 +0300

    Mean Jaccard coefficient statistics are now computed from eQTls and pQTLs datasets and plotted accordingly to the given q-value threshold

--- src/identify_QTLs.R
+++ src/identify_QTLs.R
@@ -3,7 +3,7 @@
 # 
 # Be sure to use an up to date version of R and Matrix eQTL.
 
-identify_QTLs <- function(genotypes_filename, expression_filename, output_filename) {
+identify_QTLs <- function(genotypes_filename, expression_filename, output_filename, P_THRESHOLD = 1e-2) {
   library(MatrixEQTL)
   
   ## Location of the package with the data files.
@@ -20,19 +20,24 @@ identify_QTLs <- function(genotypes_filename, expression_filename, output_filena
   # Gene expression file name
   full_expression_filename = paste(base.dir, paste("/data/", expression_filename, sep=""), sep="");
   # Output file name
-  full_output_filename = paste(base.dir, paste("/data/", output_filename, sep=""), sep="");
+  full_output_filename = paste(base.dir, paste("/data/1", output_filename, sep=""), sep="");
+  if (!file.exists(full_output_filename)) {
+    file.create(full_output_filename)
+  }
   
   # Only associations significant at this level will be saved
-  pvOutputThreshold = 1e-3;
+  pvOutputThreshold = P_THRESHOLD;
   
   ## Load genotype data
+  
+  ## 
   
   snps = SlicedData$new();
   snps$fileDelimiter = '\t';      # the TAB character
   snps$fileOmitCharacters = "NA"; # denote missing values;
   snps$fileSkipRows = 1;          # one row of column labels
   snps$fileSkipColumns = 1;       # one column of row labels
-  snps$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+  snps$fileSliceSize = 5000;      # read file in slices of 2,000 rows
   snps$LoadFile(full_genotypes_filename);
   
   ## Load gene expression data
@@ -42,7 +47,7 @@ identify_QTLs <- function(genotypes_filename, expression_filename, output_filena
   gene$fileOmitCharacters = "NA"; # denote missing values;
   gene$fileSkipRows = 1;          # one row of column labels
   gene$fileSkipColumns = 1;       # one column of row labels
-  gene$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+  gene$fileSliceSize = 5000;      # read file in slices of 2,000 rows
   gene$LoadFile(full_expression_filename);
   
   ## Run the analysis
@@ -55,18 +60,27 @@ identify_QTLs <- function(genotypes_filename, expression_filename, output_filena
     useModel = useModel,
     verbose = TRUE,
     pvalue.hist = TRUE,
-    min.pv.by.genesnp = FALSE,
-    noFDRsaveMemory = FALSE);
+    min.pv.by.genesnp = TRUE,
+    noFDRsaveMemory = FALSE
+  );
   
   ## Results:
   
   cat('Analysis done in: ', me$time.in.sec, ' seconds', '\n');
-  cat('Detected QTLs:', '\n');
   
   ## Plot the histogram of all p-values
-  
-  plot(me)
+  #plot(me)
 }
 
-identify_QTLs('rna_genotypes.csv', 'rna_expression_avg.csv', "eQTLs.csv")
-identify_QTLs('protein_genotypes.csv', 'protein_expression_avg.csv', "pQTLs.csv")
+args <- commandArgs(TRUE)
+
+identify_QTLs(
+  'rna_genotypes.csv',
+  'rna_expression_avg.csv',
+  'eQTLs.csv'
+)
+identify_QTLs(
+  'protein_genotypes.csv',
+  'protein_expression_avg.csv',
+  'pQTLs.csv'
+)
