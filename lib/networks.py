27a15cd8ac68a9a0bc46608f866f61e30b40254f
ivanov-v-v <vy.vl.ivanov@gmail.com>
2018-01-30 16:39:32 +0300

    created modules

---
+++ lib/networks.py
@@ -0,0 +1,70 @@
+import igraph as ig
+
+
+# Given a list of gene-gene interactions of some kind (pairs of genes treated as edges),
+# construct a graph and, if specified, randomize it, preserving some invariant
+def assemble_graph_of_interactions(edges, directed=False, randomize=False):
+    interaction_graph = ig.Graph(directed=directed)
+    # For some weird reason, igraph can add multiple copies of the same
+    # vertex without even signalling about it, therefore the duplicates
+    # require manual removal from the dataset
+    vertices = set()
+    for source, target in edges:
+        vertices |= {source, target}
+    vertex_names = list(vertices)
+    interaction_graph.add_vertices(vertex_names)
+    interaction_graph.add_edges(edges)
+    ''' TODO:   There must be hundreds to thousands of 
+                randomization iterations over which the
+                results will be averaged and used,
+                this must be implemented efficiently
+    '''
+    if randomize:
+        if directed:
+            interaction_graph = interaction_graph.Degree_Sequence(
+                interaction_graph.outdegree(),
+                interaction_graph.indegree(),
+                method='vl'
+            )
+        else:
+            interaction_graph = interaction_graph.Degree_Sequence(
+                interaction_graph.degree(),
+                method='vl'
+            )
+    # Due to domain-related specifics,
+    # directed graphs produced by this function
+    # will represent marker-gene interactions
+    # and must be bipartite therefore
+    if directed:
+        interaction_graph.vs["type"] = \
+            [False if deg > 0 else True for deg in interaction_graph.outdegree()]
+    interaction_graph.vs["name"] = vertex_names
+    return interaction_graph
+
+
+''' TODO:   Стоит попробовать более совершенные метрики
+            подобия, а также добиться лучшей скорости работы 
+'''
+
+# Given a pair of graphs, representing gene-gene interactions
+# and estimated QTL-linkages, calculate for each pair of interacting genes
+# a Jaccard similarity coefficient, and then average it over all edges
+def mean_linkage_similarity(interaction_graph, QTL_graph):
+    linked_genes = set([vertex["name"] for vertex in QTL_graph.vs])
+    interacting_genes = [vertex["name"] for vertex in interaction_graph.vs]
+
+    mean_coeff = 0.
+    # Перебрать все рёбра и сопоставить каждой вершине
+    # пару множеств: eQTLs и pQTLs, которые с ней линкуются,
+    # а затем рассмотреть меру пересечения их объединения с мерой пересечения
+    if interaction_graph.ecount():
+        for edge in interaction_graph.es:
+            s_id, t_id = edge.source, edge.target
+            s_name = interacting_genes[s_id]
+            t_name = interacting_genes[t_id]
+            if s_name in linked_genes and t_name in linked_genes:
+                s_neigh = set(QTL_graph.neighbors(s_name, mode="IN"))
+                t_neigh = set(QTL_graph.neighbors(t_name, mode="IN"))
+                mean_coeff += len(s_neigh & t_neigh) / len(s_neigh | t_neigh)
+        mean_coeff /= interaction_graph.ecount()
+    return mean_coeff
---
+++ lib/qtls.py
@@ -0,0 +1,46 @@
+import networks
+import numpy as np
+
+
+# Given a dataframe of estimated QTL linkages and
+# full strain genotype lookup table, plot the number
+# of linkages against marker location in genome
+def map_linkages_to_genome_location(QTL_df, full_genotypes_df):
+    QTL_graph = networks.assemble_graph_of_interactions(
+        edges=QTL_df[["SNP", "gene"]].values,
+        directed=True
+    )
+    # Select only the marker vertices
+    left_part = QTL_graph.vs.select(type=False)
+    # And map them to their absolute genome location
+    marker_to_rownum = \
+        dict(
+            zip(
+                full_genotypes_df.iloc[:, 0],
+                np.arange(full_genotypes_df.shape[0])
+            )
+        )
+    # In order to combine plots for different QTLs
+    # it's wise to plot them against the total set of markers,
+    # disregarding whether linkages for the given marker
+    # are calculated or not
+    QTL_marker_to_linkages = \
+        dict(
+            zip(
+                full_genotypes_df.iloc[:, 0],
+                np.zeros(full_genotypes_df.shape[0])
+            )
+        )
+    # Add the available linkage data
+    QTL_marker_to_linkages.update(dict(zip(left_part["name"], left_part.outdegree())))
+    # Sort the dictionary accordingly to the marker position on the chromosome
+    # and unzip it to extract the ordered linkage list
+
+    QTL_x, QTL_y = \
+        map(list,
+            zip(
+                *sorted(QTL_marker_to_linkages.items(),
+                        key=lambda p: marker_to_rownum[p[0]])
+            )
+            )
+    return QTL_x, QTL_y
