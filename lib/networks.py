dfb1006a981e9bc59d5c50407d735862e80ea1be
ivanov-v-v <vy.vl.ivanov@gmail.com>
2018-07-19 16:11:06 +0300

    geneontology modules are now extracted correctly

--- lib/networks.py
+++ lib/networks.py
@@ -70,6 +70,10 @@ def graph_from_edges(edges, directed=False, randomize=False):
 
 def basic_module_info(interactome_graph, modules_dict, modules_type, make_plots=True, simplify=False):
     module_graphs_dict = {}
+    if make_plots:
+        destdir = '/'.join(["./img/functional_module_graphs",
+                            modules_type,
+                            "simplified" if simplify else "raw"])
     for module_name in modules_dict.keys():
         module_graph = interactome_graph.subgraph(
             set(modules_dict[module_name])
@@ -79,9 +83,6 @@ def basic_module_info(interactome_graph, modules_dict, modules_type, make_plots=
             module_graph = module_graph.simplify()
             module_graph.vs.select(_degree=0).delete()
         if make_plots:
-            destdir = '/'.join(["./img/functional_module_graphs",
-                                modules_type,
-                                "simplified" if simplify else "raw"])
             ig.plot(
                 module_graph,
                 layout=module_graph.layout_kamada_kawai(),
@@ -89,7 +90,9 @@ def basic_module_info(interactome_graph, modules_dict, modules_type, make_plots=
                 vertex_label=module_graph.vs["name"]
             );
         module_graphs_dict[module_name] = module_graph
+
     return pd.DataFrame(
-        [(mname, mgraph.vcount(), mgraph.ecount()) for mname, mgraph in module_graphs_dict.items()],
-        columns=["module_name", "genes count", "interactions count"]
+        [(mname, len(modules_dict[mname]), mgraph.vcount(), mgraph.ecount())
+         for mname, mgraph in module_graphs_dict.items()],
+        columns=["module_name", "genes_count", "genes_with_interactions", "interactions_count"]
     )
