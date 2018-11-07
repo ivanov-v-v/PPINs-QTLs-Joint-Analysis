813e02f57ec4a7a0388f5b1099ed609af0ade6f8
ivanov-v-v <vy.vl.ivanov@gmail.com>
2018-11-07 18:39:28 +0300

    migrating on cluster

--- fs_utils.py
+++ fs_utils.py
@@ -1,7 +1,9 @@
 import os.path
 
-__all__ = ["ROOT_DIR", "LIB_DIR", "BIN_DIR"]
+__all__ = ["ROOT_DIR", "LIB_DIR", "SRC_DIR", "DATA_DIR"]
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
 LIB_DIR = os.path.join(ROOT_DIR, "lib")
-BIN_DIR = os.path.join(ROOT_DIR, "src")
+SRC_DIR = os.path.join(ROOT_DIR, "src")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
