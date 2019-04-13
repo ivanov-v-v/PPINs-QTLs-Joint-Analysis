efccde73ca92a2875524ea427fff752477dde4ac
ivanov-v-v <vy.vl.ivanov@gmail.com>
2019-04-13 10:39:18 +0300

    fixed directory name

--- src/randomize_interactome.py
+++ src/randomize_interactome.py
@@ -5,13 +5,12 @@ import pickle
 import igraph as ig
 from joblib import Parallel, delayed
 
-# os.chdir("/home/vvi/Science/eQTL_analysis/")
-os.chdir("/home/ivanov_vv/eQTL_analysis/")
+os.chdir("..")
 
 RANDITER = 1024
 
 interactions_type = "all"
-with open("./data/interactions/{}_interactions_graph.pkl".format(interactions_type), "rb") as pickle_file:
+with open("./data/raw/interactions/{}_interactions_graph.pkl".format(interactions_type), "rb") as pickle_file:
     interactome_graph = pickle.load(pickle_file)
 
 def _produce_randomized_interactome_copy(i):
@@ -22,7 +21,7 @@ def _produce_randomized_interactome_copy(i):
     randomized_interactome.vs["name"] = interactome_graph.vs["name"]
     ig.write(
         graph=randomized_interactome,
-        filename="./data/randomized_interactome_copies/{0}/{1}.pkl".format(interactions_type, i),
+        filename="./data/raw/randomized_interactome_copies/{0}/{1}.pkl".format(interactions_type, i),
         format="pickle"
     )
 
